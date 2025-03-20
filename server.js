d03311fee5c941c1c1082ccde5ad56fd4cd0f4b3
jordansanchez23 <joivisvis@hotmail.com>
2025-03-19 18:46:30 -0700

    MVC

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
 
 
 
@@ -22,7 +23,7 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.get("/", baseController.buildHome)
 
 // Index route
 app.get("/", function(req, res){
